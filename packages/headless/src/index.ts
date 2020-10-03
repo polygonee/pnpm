a1511e865a5140cc5d8f9218c9a435699d7bf69b
Zoltan Kochan <z@kochan.io>
2020-10-03 18:04:50 +0300

    refactor: wip

--- packages/headless/src/index.ts
+++ packages/headless/src/index.ts
@@ -587,7 +587,7 @@ async function lockfileToDepGraph (
       }
 
       const peerDeps = pkgSnapshot.peerDependencies ? new Set(Object.keys(pkgSnapshot.peerDependencies)) : null
-      graph[dir].children = await getChildrenPaths(ctx, allDeps, peerDeps)
+      graph[dir].children = await getChildrenPaths(ctx, allDeps, peerDeps, '.')
     }
     for (const importerId of opts.importerIds) {
       const projectSnapshot = lockfile.importers[importerId]
@@ -596,7 +596,7 @@ async function lockfileToDepGraph (
         ...(opts.include.dependencies ? projectSnapshot.dependencies : {}),
         ...(opts.include.optionalDependencies ? projectSnapshot.optionalDependencies : {}),
       }
-      directDependenciesByImporterId[importerId] = await getChildrenPaths(ctx, rootDeps, null)
+      directDependenciesByImporterId[importerId] = await getChildrenPaths(ctx, rootDeps, null, importerId)
     }
   }
   return { graph, directDependenciesByImporterId }
@@ -616,13 +616,14 @@ async function getChildrenPaths (
     storeController: StoreController
   },
   allDeps: {[alias: string]: string},
-  peerDeps: Set<string> | null
+  peerDeps: Set<string> | null,
+  importerId: string
 ) {
   const children: {[alias: string]: string} = {}
   for (const alias of Object.keys(allDeps)) {
     const childDepPath = dp.refToAbsolute(allDeps[alias], alias, ctx.registries)
     if (childDepPath === null) {
-      children[alias] = path.resolve(ctx.lockfileDir, allDeps[alias].substr(5))
+      children[alias] = path.resolve(ctx.lockfileDir, importerId, allDeps[alias].substr(5))
       continue
     }
     const childRelDepPath = dp.refToRelative(allDeps[alias], alias) as string
